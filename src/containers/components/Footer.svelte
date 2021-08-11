1c1aea121da75a3151cf4a6ff4c8add3a7195925
Marco Morales Lopez <marco.02lopez@hotmail.com>
2021-08-10 19:38:33 -0500

    agregando los demas archivos

---
+++ src/containers/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<!-- Footer.svelte -->
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+  
+  <div class="Footer">
+    <footer class="Footer-container">
+      <p class="Footer-copy">&copy{currentYear} Enrique Moreno</p>
+    </footer>
+  </div>
---
+++ src/containers/components/Sidebar.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Profile from './Profile.svelte';
+    import Stories from './Stories.svelte';
+    import Footer from './Footer.svelte';
+
+    export let nickname;
+    export let name;
+</script>
+
+<!-- Sidebar.svelte -->
+<style>
+    .Sidebar {
+      position: relative;
+      padding: 6.5em 0 0 0;
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+    }
+    .Sidebar-container {
+      position: relative;
+    }
+</style>
+
+<div class="Sidebar">
+    <div class="Sidebar-container">
+        <Profile {nickname} {name} />
+        <Stories />
+        <Footer />
+    </div>
+</div>
